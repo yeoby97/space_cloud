65663c090b979ce365a6758cac5ea44de191f0b8
jiuk0304 <jiuk0304@naver.com>
2025-03-24 23:03:54 +0900

    firebase_options를 firebase 폴더에 정리.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
