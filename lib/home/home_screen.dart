001b9beacf2cf805acebb24afd093069df8c14b7
jiuk0304 <jiuk0304@naver.com>
2025-03-25 01:45:24 +0900

    네이버 지도 API 추가 및 HomeScreen에 네이버 지도 띄우기 시도.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
 class HomeScreen extends StatelessWidget {
   final User? _user; // 생성자로 user를 받아옴
@@ -16,22 +17,8 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(_user?.photoURL ?? ''), // null 처리를 해줌
-              radius: 40,
-            ),
-            const SizedBox(height: 10),
-            Text('이름: ${_user?.displayName ?? '알 수 없음'}'), // null 처리
-            Text('이메일: ${_user?.email ?? '알 수 없음'}'), // null 처리
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // 로그아웃 처리
-              },
-              child: const Text('로그아웃'),
-            )
+            NaverMap(),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:space_cloud/sign/signin/signin_screen.dart';
 import 'firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 파이어베이스 초기화
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // 네이버 지도 초기화
+  await NaverMapSdk.instance.initialize(
+      clientId: '2t4gtv24kh',
+      onAuthFailed: (error) {
+        print('Auth failed: $error');
+      });
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SignInScreen(),
     ),
-  ); // SigninScreen()
+  );
 }
 
