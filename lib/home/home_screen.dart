2f7be202ab1c0165c9ccc31e0b98eae5ce7d3d73
yeoby97 <hydudduq@naver.com>
2025-03-25 18:09:09 +0900

    구글지도

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
-  final User? _user; // 생성자로 user를 받아옴
-
-  // 생성자에서 User 객체를 받아옴
+class HomeScreen extends StatefulWidget {
+  final User? _user;
   const HomeScreen({
     required User? user,
     super.key,
   }) : _user = user;
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+  @override
   Widget build(BuildContext context) {
     // _user가 null이 아니면 사용자 정보 출력
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            NaverMap(),
-          ],
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
         ),
       ),
     );
