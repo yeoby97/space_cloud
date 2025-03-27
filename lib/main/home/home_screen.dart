711d5ecbaee1fbae837f09144834ade8ac41d046
jiuk0304 <jiuk0304@naver.com>
2025-03-28 00:52:15 +0900

    home_screen에 searchBox, SearchScreen 제작.

--- lib/main/home/home_screen.dart
+++ lib/main/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:space_cloud/main/home/search_screen.dart';
 
 import 'my_location_view_model.dart';
 
@@ -21,10 +22,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Map Example")),
       body: Stack(
         children: [
           _googleMap(),
+          _SearchBox(onTap: _onTap,),
           _floatingButton(),
         ]
       ),
@@ -64,8 +65,11 @@ class _HomeScreenState extends State<HomeScreen> {
     return Align(
       alignment: Alignment.bottomLeft,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: FloatingActionButton(
+          shape: CircleBorder(),
+          foregroundColor: Colors.black.withAlpha(150),
+          backgroundColor: Colors.white,
           onPressed: () {
             _goToCurrentLocation();
           },
@@ -86,5 +90,79 @@ class _HomeScreenState extends State<HomeScreen> {
         zoom: 18.0,
       ),
     ));
+  }
+
+  void _onTap() async{
+    final LatLng result = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          return SearchScreen();
+        },
+      ),
+    );
+    _mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(result.latitude, result.longitude),
+        zoom: 18.0,
+      ),
+    ));
+  }
+}
+
+class _SearchBox extends StatelessWidget {
+  final VoidCallback onTap;
+  const _SearchBox({super.key,required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: GestureDetector(
+            onTap: onTap,
+            child: Container(
+              width: 500,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2), // 그림자 색상
+                    blurRadius: 10,   // 흐림 정도
+                    spreadRadius: 2,  // 그림자 확산 정도
+                    offset: Offset(3, 3), // X, Y 방향 위치 조정
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.map),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          '장소, 위치 검색',
+                          style: TextStyle(
+                            color: Colors.black.withAlpha(100),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.search),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );;
   }
 }
---
+++ lib/main/home/search_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+
+class SearchScreen extends StatefulWidget {
+
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController controller = TextEditingController();
+  String? selectedAddress;
+  String? selectedLatLng;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView( // 스크롤 가능하게
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            placesAutoCompleteTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  placesAutoCompleteTextField() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          child: GooglePlaceAutoCompleteTextField(
+            boxDecoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2), // 그림자 색상
+                  blurRadius: 10,   // 흐림 정도
+                  spreadRadius: 2,  // 그림자 확산 정도
+                  offset: Offset(3, 3), // X, Y 방향 위치 조정
+                ),
+              ],
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            textEditingController: controller,
+            googleAPIKey: 'AIzaSyAuhd1aQTSgjtgnydP3_wgD3SDD2QD-VGU',
+            inputDecoration: InputDecoration(
+              hintText: "Search your location",
+              border: InputBorder.none,
+              enabledBorder: InputBorder.none,
+            ),
+            debounceTime: 200,
+            countries: ["kr"],
+            isLatLngRequired: true,
+            getPlaceDetailWithLatLng: (Prediction prediction) {
+              LatLng location = LatLng(prediction.lat as double,prediction.lng as double);
+              Navigator.of(context).pop(location);
+            },
+            itemClick: (Prediction prediction) {
+              controller.text = prediction.description ?? "";
+              controller.selection = TextSelection.fromPosition(
+                  TextPosition(offset: prediction.description?.length ?? 0));
+            },
+            seperatedBuilder: Divider(),
+            containerHorizontalPadding: 10,
+            itemBuilder: (context, index, Prediction prediction) {
+              return Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.location_on),
+                    SizedBox(width: 7),
+                    Expanded(child: Text("${prediction.description ?? ""}"))
+                  ],
+                ),
+              );
+            },
+            isCrossBtnShown: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
