05457c456aef33d15e928aa3c2a27d375aea0fcd
yeoby97 <hydudduq@naver.com>
2025-05-09 16:58:28 +0900

    Revert "my_location 정상화"
    This reverts commit 6ff5ab20af903d97203b32ac8faea1e2005f41ce.

--- lib/main/home/my_location/my_location.dart
+++ lib/main/home/my_location/my_location.dart
@@ -1,8 +1,12 @@
+// TODO : 최적화 및 상태 최상단화
+
 import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 
 class MyLocation {
-  final _locationController = StreamController<Position>.broadcast();
+  final StreamController<Position> _locationController =
+  StreamController<Position>.broadcast();
+
   StreamSubscription<Position>? _positionSubscription;
 
   MyLocation() {
@@ -15,7 +19,9 @@ class MyLocation {
         accuracy: LocationAccuracy.high,
         distanceFilter: 5,
       ),
-    ).listen(_locationController.add);
+    ).listen((Position position) {
+      _locationController.add(position);
+    });
   }
 
   Stream<Position> get positionStream => _locationController.stream;
--- lib/main/home/my_location/my_location_view_model.dart
+++ lib/main/home/my_location/my_location_view_model.dart
@@ -15,22 +15,20 @@ class MyLocationViewModel extends ChangeNotifier {
   Position? get currentPosition => _currentPosition;
 
   Future<void> _initialize() async {
+    // 앱 시작 시 최초 위치 가져오기
     try {
-      _currentPosition = await Geolocator.getCurrentPosition(
-        locationSettings: const LocationSettings(
-          accuracy: LocationAccuracy.high,
-        ),
-      );
+      _currentPosition = await Geolocator.getCurrentPosition();
     } catch (e) {
       print("초기 위치 가져오기 실패: $e");
     }
 
+    // 위치 스트림 구독
     _subscription = _myLocation.positionStream.listen((position) {
       _currentPosition = position;
       notifyListeners();
     });
 
-    notifyListeners();
+    notifyListeners(); // 초기 위치 반영
   }
 
   @override
